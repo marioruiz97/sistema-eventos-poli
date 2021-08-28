701c3e26b8a351ee3905709d08e6239fe422e05c
marirubd <mario.ruiz@ceiba.com.co>
2021-08-27 21:33:54 -0500

    Commit inicial

---
+++ src/main/java/edu/politecnicojic/eventos/SistemaDeEventosPoliApplication.java
@@ -0,0 +1,13 @@
+package edu.politecnicojic.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEventosPoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEventosPoliApplication.class, args);
+	}
+
+}
